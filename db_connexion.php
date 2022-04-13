c70840f7c1b0a3f80bd36dcda3ee19dd87071c3c
Moustapha <kammoun.moustapha@gmail.com>
2022-04-13 09:40:58 +0200

    Database connexion file

---
+++ db_connexion.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_servername = "localhost";
+$db_dbname = "db_project-portfolio";
+$db_username = "phpmyadmin";
+$db_password = "";
+
+try {
+    $db = new PDO("mysql:host=$db_servername;dbname=$db_dbname", $db_username, $db_password);
+        // set the PDO error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
